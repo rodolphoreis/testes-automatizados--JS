4c52adc7cb546094ef2c0b591e55493e06f2190e
Rodolpho Reis <rodolphoreis@live.com>
2024-02-01 17:32:24 +0000

    feat(test): implementa teste de pesquisa de herói utilizando arquivos
    - Adiciona teste para a função de pesquisa em 'test.js'
    - Atualiza 'database.js' para incluir a função de escrita no arquivo
    - Cria o arquivo 'herois.json' com um herói de exemplo

---
+++ cli/test.js
@@ -0,0 +1,24 @@
+const { deepEqual, ok } = require("assert");
+
+const database = require("./database");
+
+const DEFAULD_ITEM_CADASTRAR = {
+  nome: "Flash",
+  poder: "Speed",
+  id: 1,
+};
+
+describe("Suite de manipulação de Herois", () => {
+  it("Deve pesquisar um heroi usando aquivos", async () => {
+    const expected = DEFAULD_ITEM_CADASTRAR;
+    const [resultado] = await database.listar(expected.id);
+
+    deepEqual(resultado, expected);
+  });
+
+  /*  it("Deve cadastrar um heroi, usando arquivos", async () => {
+    const expected = DEFAULD_ITEM_CADASTRAR;
+
+    ok(null, expected);
+  }); */
+});
